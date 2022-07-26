c6b6731b0a01ff0374c1e28e42bfd383fbda5023
yodarjeering <yodarjeering429@outlook.com>
2022-07-26 20:57:32 +0900

    torima

---
+++ 21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+// using ll = long long;
+
+
+int main(void){
+
+    int N,M;
+    cin>>N>>M;
+    vector<int> A;
+    vector<int> B;
+
+    cout<<N<<endl;
+    cout<<M<<endl;
+
+    return 0;
+}
